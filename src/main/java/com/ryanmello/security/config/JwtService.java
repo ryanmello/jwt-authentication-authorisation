8c774d95d01aa24fe78482d274a230583ef373c4
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-02-07 10:24:00 -0800

    jwt service

---
+++ src/main/java/com/ryanmello/security/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.ryanmello.security.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+}
